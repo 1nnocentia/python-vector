9bb7e102a70c572a4b74d488f786fefd9863fe06
Innocentia <innocentiahan@gmail.com>
2024-11-13 11:19:42 +0800

    membuat fungsi untuk menghitung unit vektor

--- vector-feature/unit_vector.py
+++ vector-feature/unit_vector.py
@@ -1,1 +1,8 @@
 #vector unit
+import math
+def unit_vector(v):
+    #panjang = magnitudo
+    panjang = math.sqrt(sum([i**2 for i in v]))
+    unit_vector = [i/panjang for i in v]
+    unit_vector = [round(i,2) for i in unit_vector]
+    return unit_vector
