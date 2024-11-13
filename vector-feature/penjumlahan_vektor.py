542c9e85f6144ba4d839fc8218e7b2aade2184d8
WyattMatt <wyatt.honny06@gmail.com>
2024-11-13 11:48:30 +0800

    Update penjumlahan_vektor.py
    Saya memberikan perubahan pada penjumlahan_vektor.py

--- vector-feature/penjumlahan_vektor.py
+++ vector-feature/penjumlahan_vektor.py
@@ -1,7 +1,6 @@
 # Penjumlahan Vektor
 
-def penjumlahan_vektor(x, y):
-    hasil_x = x[0] + y[0]
-    hasil_y = x[1] + y[1]
-    hasil = [hasil_x, hasil_y]
-    return hasil
+def plus(a, b):
+    result1 = a[0] + b[0]
+    result2 = a[1] + b[1]
+    return [result1, result2]
