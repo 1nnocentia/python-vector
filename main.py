2098aadf144854ab2a5484606f8bd8b979afd894
WyattMatt <wyatt.honny06@gmail.com>
2024-11-15 13:51:11 +0800

    Update kedua main.py
    Melakukan revisi kedua pada file main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from func_addition import plus
-from func_subtraction import mines
-from func_multiplication import times
-from func_length import length
-from func_unit import unit
+from penjumlahan_vector import penjumlahan_vector
+from pengurangan_vector import pengurangan_vector
+from dot_product import dot_product
+from panjang_vector import panjang_vector
+from unit_vector import unit_vector
 import os
 import time
 
@@ -16,25 +16,25 @@ while True:
     if pilihan == "1":
         a = list(map(int, input("Masukkan Vektor A : ").split(",")))
         b = list(map(int, input("Masukkan Vektor B : ").split(",")))
-        print(f"Hasil penjumlahan Vektor A dan B adalah", plus(a, b))
+        print(f"Hasil penjumlahan Vektor A dan B adalah", penjumlahan_vector(a, b))
 
     elif pilihan == "2":
         a = list(map(int, input("Masukkan Vektor A : ").split(",")))
         b = list(map(int, input("Masukkan Vektor B : ").split(",")))
-        print(f"Hasil pengurangan Vektor A dan B adalah", mines(a, b))
+        print(f"Hasil pengurangan Vektor A dan B adalah", pengurangan_vector(a, b))
 
     elif pilihan == "3":
         a = list(map(int, input("Masukkan Vektor A : ").split(",")))
         b = list(map(int, input("Masukkan Vektor B : ").split(",")))
-        print(f"Hasil perkalian Vektor A dan B adalah", times(a, b))
+        print(f"Hasil perkalian Vektor A dan B adalah", dot_product(a, b))
 
     elif pilihan == "4":
         a = list(map(int, input("Masukkan Vektor A : ").split(",")))
-        print(f"Hasil panjang Vektor A adalah", length(a))
+        print(f"Hasil panjang Vektor A adalah", panjang_vector(a))
 
     elif pilihan == "5":
         a = list(map(int, input("Masukkan Vektor A : ").split(",")))
-        print(f"Hasil unit Vector A adalah", unit(a))
+        print(f"Hasil unit Vector A adalah", unit_vector(a))
         
     else:
         print("Pilihan tidak valid.")
