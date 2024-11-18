1f57d2bcbaa62b1d998558a0718ff4a8cc7f9de5
Arsya Aulia Amira <aauliaamira@student.ciputra.ac.id>
2024-11-18 20:59:28 +0800

    Melakukan perubahan file panjang_vector

--- vector-feature/panjang_vector.py
+++
@@ -1,4 +0,0 @@
-#menghitung panjang vector
-def panjang_vektor(a, b):
-    panjang = (a[0]**2 + a[1]**2)**0.5 
-    return panjang
--- vector_feature/panjang_vector.py
+++ vector_feature/panjang_vector.py
@@ -1,4 +1,4 @@
 #menghitung panjang vector
-def panjang_vektor(a, b):
+def panjang_vektor(a):
     panjang = (a[0]**2 + a[1]**2)**0.5 
     return panjang
