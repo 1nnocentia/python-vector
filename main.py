7ebf3a429cd1c8215064f8516dafc4305af664e9
Innocentia <innocentiahan@gmail.com>
2024-11-18 15:36:57 +0800

    mengubah nama folder dan update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from penjumlahan_vector import penjumlahan_vector
-from pengurangan_vector import pengurangan_vector
-from dot_product import dot_product
-from panjang_vector import panjang_vector
-from unit_vector import unit_vector
+from vector_feature.penjumlahan_vektor import plus
+from vector_feature.pengurangan_vector import pengurangan
+from vector_feature.dot_product import dot_product
+from vector_feature.panjang_vector import panjang_vektor
+from vector_feature.unit_vector import unit_vector
 import os
 import time
 
@@ -16,12 +16,12 @@ while True:
     if pilihan == "1":
         a = list(map(int, input("Masukkan Vektor A : ").split(",")))
         b = list(map(int, input("Masukkan Vektor B : ").split(",")))
-        print(f"Hasil penjumlahan Vektor A dan B adalah", penjumlahan_vector(a, b))
+        print(f"Hasil penjumlahan Vektor A dan B adalah", plus(a, b))
 
     elif pilihan == "2":
         a = list(map(int, input("Masukkan Vektor A : ").split(",")))
         b = list(map(int, input("Masukkan Vektor B : ").split(",")))
-        print(f"Hasil pengurangan Vektor A dan B adalah", pengurangan_vector(a, b))
+        print(f"Hasil pengurangan Vektor A dan B adalah", pengurangan(a, b))
 
     elif pilihan == "3":
         a = list(map(int, input("Masukkan Vektor A : ").split(",")))
@@ -30,7 +30,7 @@ while True:
 
     elif pilihan == "4":
         a = list(map(int, input("Masukkan Vektor A : ").split(",")))
-        print(f"Hasil panjang Vektor A adalah", panjang_vector(a))
+        print(f"Hasil panjang Vektor A adalah", panjang_vektor(a))
 
     elif pilihan == "5":
         a = list(map(int, input("Masukkan Vektor A : ").split(",")))
